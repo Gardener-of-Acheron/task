250b57fefd11b9df7096c1131cb58b31d6ab983e
Pete Davison <pd93.uk@outlook.com>
2022-09-29 17:13:57 +0000

    feat: add coloured output to --list and --list-all

--- help.go
+++ help.go
@@ -42,9 +42,13 @@ func (e *Executor) printTasks(listAll bool) {
 	e.Logger.Outf(logger.Default, "task: Available tasks for this project:")
 
 	// Format in tab-separated columns with a tab stop of 8.
-	w := tabwriter.NewWriter(e.Stdout, 0, 8, 0, '\t', 0)
+	w := tabwriter.NewWriter(e.Stdout, 0, 8, 6, ' ', 0)
 	for _, task := range tasks {
-		fmt.Fprintf(w, "* %s: \t%s\n", task.Name(), task.Desc)
+		fmt.Fprintf(w, "* ")
+		e.Logger.FOutf(w, logger.Green, task.Name())
+		fmt.Fprintf(w, ": \t")
+		e.Logger.FOutf(w, logger.Cyan, task.Desc)
+		fmt.Fprintf(w, "\n")
 	}
 	w.Flush()
 }
